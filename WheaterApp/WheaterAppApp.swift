e52bab770158c90f3bd5c1a4ad1909c8c010ac52
andres florez <andresflopaez@gmail.com>
2021-12-15 10:36:14 -0500

    Initial Commit

---
+++ WheaterApp/WheaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheaterAppApp.swift
+//  WheaterApp
+//
+//  Created by Andrés Flórez on 15/12/21.
+//
+
+import SwiftUI
+
+@main
+struct WheaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
